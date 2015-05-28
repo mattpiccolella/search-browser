61ea9f2aa010187cf9c821581bd3dfaa272bacd6
mjp2220 <mjp2220@columbia.edu>
2015-05-28 00:05:59 -0700

    Finishing up

--- SearchBrowser/ViewController.swift
+++ SearchBrowser/ViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UISearchBarDelegate {
     
     var searchMode: Bool = false
-    @IBOutlet var verticalSearchCenter: NSLayoutConstraint!
-    @IBOutlet var searchBox: UITextField!
+    let topOffset: CGFloat = 260
+    let webViewOffset: CGFloat = 100
+    let transitionDelay: NSTimeInterval = 0.2
+    var webView: UIWebView!
+
+    @IBOutlet var verticalCenterConstraint: NSLayoutConstraint!
+    @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var appLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.searchBar.showsCancelButton = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,39 +30,53 @@ class ViewController: UIViewController,UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func textFieldDidBeginEditing(textField: UITextField) {
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
         self.appLabel.hidden = true
-    
-        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            
+        println("HERE")
+        self.webView?.removeFromSuperview()
+        
+        UIView.animateWithDuration(self.transitionDelay, animations: { () -> Void in
             self.searchMode = true
-            let offset: CGFloat = self.searchMode ? 250.0 : 0.0
-            self.verticalSearchCenter.constant = offset
-        }, completion: { (value:Bool) -> Void in
-            println("Finished")
+            self.transitionSearchBar()
+            }, completion: { (value:Bool) -> Void in
+                println("Finished")
         })
     }
     
-    func textFieldDidEndEditing(textField: UITextField) {
-        self.appLabel.hidden = false
-    }
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        self.searchBox.resignFirstResponder()
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
         self.searchMode = true
         self.appLabel.hidden = true
-        let searchString = "http://www.google.com/search?q=" + self.searchBox.text
+        let searchString = "http://www.google.com/search?q=" + self.searchBar.text
         let encodedString = searchString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
         let searchURL:NSURL? = NSURL(string:encodedString!)
-        let webView:UIWebView = UIWebView(frame: CGRectMake(0, 100, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height-100))
+        let webView:UIWebView = UIWebView(frame: CGRectMake(0, self.webViewOffset, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height-self.webViewOffset))
         webView.loadRequest(NSURLRequest(URL: searchURL!))
-        UIView.animateWithDuration(0.1, animations: { () -> Void in
-            self.verticalSearchCenter.constant = 250
-            println(searchURL)
+        UIView.animateWithDuration(self.transitionDelay, animations: { () -> Void in
+            self.transitionSearchBar()
             self.view.addSubview(webView)
+            self.webView = webView
             }, completion: { (value:Bool) -> Void in
-                println("\(self.searchBox.center.y)")
+
         })
-        return true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        self.searchMode = false
+        UIView.animateWithDuration(self.transitionDelay, animations: { () -> Void in
+            self.searchBar.text = ""
+            self.appLabel.hidden = false
+            self.transitionSearchBar()
+        }, completion: { (value: Bool) -> Void in
+            
+        })
+    }
+    
+    func transitionSearchBar() {
+        let offset: CGFloat = self.searchMode ? self.topOffset : 0.0
+        self.verticalCenterConstraint.constant = offset
     }
 
 }
