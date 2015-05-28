d78f2cd92b6e21bf9f91fa8e2b737c49b543cded
mjp2220 <mjp2220@columbia.edu>
2015-05-27 23:31:01 -0700

    Got basic version working

--- SearchBrowser/ViewController.swift
+++ SearchBrowser/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController,UITextFieldDelegate {
-
+    
+    var searchMode: Bool = false
+    @IBOutlet var verticalSearchCenter: NSLayoutConstraint!
     @IBOutlet var searchBox: UITextField!
     @IBOutlet var appLabel: UILabel!
     override func viewDidLoad() {
@@ -23,6 +25,14 @@ class ViewController: UIViewController,UITextFieldDelegate {
     
     func textFieldDidBeginEditing(textField: UITextField) {
         self.appLabel.hidden = true
+    
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.searchMode = true
+            let offset: CGFloat = self.searchMode ? 250.0 : 0.0
+            self.verticalSearchCenter.constant = offset
+        }, completion: { (value:Bool) -> Void in
+            println("Finished")
+        })
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
@@ -31,8 +41,20 @@ class ViewController: UIViewController,UITextFieldDelegate {
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.searchBox.resignFirstResponder()
-        
+        self.searchMode = true
         self.appLabel.hidden = true
+        let searchString = "http://www.google.com/search?q=" + self.searchBox.text
+        let encodedString = searchString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        let searchURL:NSURL? = NSURL(string:encodedString!)
+        let webView:UIWebView = UIWebView(frame: CGRectMake(0, 100, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height-100))
+        webView.loadRequest(NSURLRequest(URL: searchURL!))
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.verticalSearchCenter.constant = 250
+            println(searchURL)
+            self.view.addSubview(webView)
+            }, completion: { (value:Bool) -> Void in
+                println("\(self.searchBox.center.y)")
+        })
         return true
     }
 
